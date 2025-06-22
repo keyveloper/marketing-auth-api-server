f6eb1d3dd8fb1f4c88acb5d184b8c95607560140
zen <didehdrb21@naver.com>
2025-06-22 11:50:03 +0900

    Add Dependencies: Cognito & Security

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
+
 }
 
 group = "org.example"
@@ -20,11 +21,48 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+
+
+    // monitoring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Logger
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+
+    // test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-core:5.3.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    // json
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+    // kotlin date time
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+
+    // ⚒️⚒️ Security 🔥🔥
+    // --- Spring Boot Web / Security / OAuth2 ---
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    // --- AWS SDK: Cognito User Pool ---
+    implementation("software.amazon.awssdk:cognitoidentityprovider")
+
+    // --- Validation ---
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.passay:passay:1.6.1")
 }
 
 kotlin {
--- src/test/kotlin/org/example/authapiserver/AuthApiServerApplicationTests.kt
+++ src/test/kotlin/org/example/authapiserver/AuthApiServerApplicationTests.kt
@@ -3,11 +3,9 @@ package org.example.authapiserver
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class AuthApiServerApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
